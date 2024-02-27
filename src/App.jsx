254700cd0067c3ef5fb491f9f5c33a5aeac04608
dylan horyza <horyza.dylan@gmail.com>
2024-02-27 13:33:43 -0700

    added logos and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import { Outlet } from 'react-router-dom';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 // App that renders all content 
 function App() {
   return (
     <div className='app-wrapper'>
       <div className='app-container'>
-       
+        <Header /> 
         <Outlet />
       </div>
       
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import logoImage from '../assets/SOLOVISUALSblk.png'
+import '../styles/header.css'
+export default function Header() {
+  return (
+    <div>
+      
+      <div className="header-image">
+      <img src={logoImage} alt="logo" className="logo-header"/>
+      </div>
+      <Navbar />
+    </div>
+  );
+}
