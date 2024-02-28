439b56c7c70c4f5ba9ab566bd20872fdb5e4999a
dylan horyza <horyza.dylan@gmail.com>
2024-02-28 14:28:36 -0700

    updated homepage layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,11 @@ import '../styles/homepage.css';
 import avatarImage from '../assets/beau-avatar.jpg';
 export default function HomePage() {
   return (
-    <section className='container home-container' id='home'>
-    
+    <div className='home-container' id='home'>
       <div className="youtube-video">
             <iframe className='youtube-box'
               width="100%"
-              height="500"
+              height="800"
               src="https://www.youtube.com/embed/F79OaL7Cn8Q"
               title="YouTube video player"
               frameborder="0"
@@ -15,7 +14,23 @@ export default function HomePage() {
               allowFullScreen
             ></iframe>
           </div>
+      <div className='home-music-videos'>
+        <div className='title-container'>
+        <h2 className='section-title'>Music Videos</h2>
+        <p className='section-description'>Description goes here</p>
+        <button>Watch</button>
+        </div>
+        
+      </div>
+      <div className='home-concert-videos'>
+        <div className='title-container-right'>
+        <h2 className='section-title'>Concerts</h2>
+        <p className='section-description'>Description goes here</p>
+        <button>Watch</button>
+        </div>
+        
+      </div>
+    </div>
     
-    </section>
   );
 }
