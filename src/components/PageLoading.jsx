badd0a6e411be44a5adf2fca8abbadf993f9a1f0
dylan horyza <horyza.dylan@gmail.com>
2024-03-05 14:35:36 -0700

    added photo deck animation

---
+++ src/components/PageLoading.jsx
@@ -0,0 +1,14 @@
+// LoadingComponent.jsx
+import React from 'react';
+import '../styles/pageloading.css'
+
+const PageLoading = () => {
+  return (
+    <div className="loading-container">
+      {/* Add your loading animation here */}
+      Loading...
+    </div>
+  );
+};
+
+export default PageLoading;
